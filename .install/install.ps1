b82d625da045c786a45b4c7f34f8896e96f20db2
mueller-lehner <joel.mueller@lehner-versand.ch>
2023-12-15 14:30:33 +0100

    added location and package

--- .install/install.ps1
+++ .install/install.ps1
@@ -4,7 +4,7 @@
 $installWinget = Install-WinGet
 
 if ($installWinget -eq $true){
-    winget import -i ".\winget-export.json" --accept-package-agreements
+    winget import -i ".\winget-export.json" --accept-package-agreements --location $Env:Programfiles
 } else {
     Write-Host "Couldn't Install Winget and Apps!"
 }
